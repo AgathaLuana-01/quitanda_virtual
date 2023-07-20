7bf006fc20e557b5d5dc28ef631aa66dc16a3aa1
AgathaLuana-01 <agathaluana.ads@gmail.com>
2023-07-20 18:21:34 -0300

    barra de pesquisa

--- lib/src/auth/sing_in_screen.dart
+++ lib/src/auth/sing_in_screen.dart
@@ -110,7 +110,7 @@ class SingInScreen extends StatelessWidget {
                         onPressed: () {
                           Navigator.of(context)
                               .pushReplacement(MaterialPageRoute(builder: (c) {
-                            return  BaseScreen();
+                            return const BaseScreen();
                           }));
                         },
                         child: const Text(
--- lib/src/home/home_tab.dart
+++ lib/src/home/home_tab.dart
@@ -59,11 +59,41 @@ class HomeTab extends StatelessWidget {
         ],
       ),
 
-      //Campo de pesquisa
+      body: Column(
+        children: [
+//Campo de pesquisa
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: TextFormField(
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: Colors.white,
+                isDense: true,
+                hintText: 'Pesquise aqui...',
+                hintStyle: TextStyle(
+                  color: Colors.grey.shade400,
+                  fontSize: 14,
+                ),
+                prefixIcon: Icon(
+                  Icons.search,
+                  color: CustomColors.customContrastColor,
+                ),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(60),
+                  borderSide: const BorderSide(
+                    width: 0,
+                    style: BorderStyle.none,
+                  ),
+                ),
+              ),
+            ),
+          ),
 
-      //Categoria
+          //Categoria
 
-      //Grid
+          //Grid
+        ],
+      ),
     );
   }
 }
