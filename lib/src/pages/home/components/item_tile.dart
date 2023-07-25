2828685e41f7c4df05b35e386d3923641605c343
AgathaLuana-01 <agathaluana.ads@gmail.com>
2023-07-25 18:02:14 -0300

    criação do card dos produtos

---
+++ lib/src/pages/home/components/item_tile.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+import 'package:quitanda_virtual/src/config/custom_colors.dart';
+import 'package:quitanda_virtual/src/models/item_model.dart';
+import 'package:quitanda_virtual/src/services/utils_services.dart';
+
+class ItemTile extends StatelessWidget {
+  //Receber a instancia do Item Model
+  final ItemModel item;
+
+  ItemTile({
+    Key? key,
+    required this.item,
+  }) : super(key: key);
+
+  UtilsServices ultilService = UtilsServices();
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Card(
+          elevation: 1,
+          shadowColor: Colors.grey.shade300,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(12),
+            child: Column(
+              crossAxisAlignment:
+                  CrossAxisAlignment.stretch, //Para q os textos "estiquem"
+              children: [
+                //Imagem
+                Expanded(
+                  child: Image.asset(item.imgUrl),
+                ),
+                //Nome
+                Text(
+                  item.itemName,
+                  style: const TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                //Preço - unidade
+                Row(
+                  children: [
+                    Text(
+                      ultilService.priceToCurrency(item.price),
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: CustomColors.customSwatchColor,
+                      ),
+                    ),
+                    Text(
+                      '/${item.unit}',
+                      style: TextStyle(
+                        color: Colors.grey.shade500,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 12,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+        Positioned(
+          top: 4,
+          right: 4,
+          child: GestureDetector(
+            onTap: () {},
+            child: Container(
+              height: 40,
+              width: 35,
+              decoration: BoxDecoration(
+                color: CustomColors.customSwatchColor,
+                borderRadius: const BorderRadius.only(
+                  bottomLeft: Radius.circular(15),
+                  topRight: Radius.circular(20),
+                ),
+              ),
+              child: const Icon(
+                Icons.add_shopping_cart_outlined,
+                color: Colors.white,
+                size: 20,
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
