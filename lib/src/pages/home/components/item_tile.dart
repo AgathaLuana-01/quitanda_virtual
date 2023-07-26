a17a300de2546049a2fbacaa8ead5e4f7c06f702
AgathaLuana-01 <agathaluana.ads@gmail.com>
2023-07-25 22:42:20 -0300

    GestureDector + animação da imagem, fazndo com que o card fique em tela cheia

--- lib/src/pages/home/components/item_tile.dart
+++ lib/src/pages/home/components/item_tile.dart
@@ -1,72 +1,94 @@
 import 'package:flutter/material.dart';
 import 'package:quitanda_virtual/src/config/custom_colors.dart';
 import 'package:quitanda_virtual/src/models/item_model.dart';
+import 'package:quitanda_virtual/src/pages/product/product_sreen.dart';
 import 'package:quitanda_virtual/src/services/utils_services.dart';
 
-class ItemTile extends StatelessWidget {
+class ItemTile extends StatefulWidget {
   //Receber a instancia do Item Model
   final ItemModel item;
 
-  ItemTile({
+  const ItemTile({
     Key? key,
     required this.item,
   }) : super(key: key);
 
+  @override
+  State<ItemTile> createState() => _ItemTileState();
+}
+
+class _ItemTileState extends State<ItemTile> {
   UtilsServices ultilService = UtilsServices();
 
   @override
   Widget build(BuildContext context) {
     return Stack(
       children: [
-        Card(
-          elevation: 1,
-          shadowColor: Colors.grey.shade300,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(20),
-          ),
-          child: Padding(
-            padding: const EdgeInsets.all(12),
-            child: Column(
-              crossAxisAlignment:
-                  CrossAxisAlignment.stretch, //Para q os textos "estiquem"
-              children: [
-                //Imagem
-                Expanded(
-                  child: Image.asset(item.imgUrl),
-                ),
-                //Nome
-                Text(
-                  item.itemName,
-                  style: const TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold,
+        // Conteúdo
+        GestureDetector(
+          onTap: () {
+            Navigator.of(context).push(
+              MaterialPageRoute(builder: (c) {
+                return  ProductSreen(item: widget.item,);
+              }),
+            );
+          },
+          child: Card(
+            elevation: 1,
+            shadowColor: Colors.grey.shade300,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(12),
+              child: Column(
+                crossAxisAlignment:
+                    CrossAxisAlignment.stretch, //Para q os textos "estiquem"
+                children: [
+
+
+                  //Imagem
+                  Expanded(
+                    child: Hero(
+                      tag: widget.item.imgUrl,
+                      child: Image.asset(widget.item.imgUrl)),
                   ),
-                ),
-                //Preço - unidade
-                Row(
-                  children: [
-                    Text(
-                      ultilService.priceToCurrency(item.price),
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20,
-                        color: CustomColors.customSwatchColor,
-                      ),
+                  //Nome
+                  Text(
+                    widget.item.itemName,
+                    style: const TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
                     ),
-                    Text(
-                      '/${item.unit}',
-                      style: TextStyle(
-                        color: Colors.grey.shade500,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 12,
+                  ),
+                  //Preço - unidade
+                  Row(
+                    children: [
+                      Text(
+                        ultilService.priceToCurrency(widget.item.price),
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                          color: CustomColors.customSwatchColor,
+                        ),
                       ),
-                    ),
-                  ],
-                ),
-              ],
+                      Text(
+                        '/${widget.item.unit}',
+                        style: TextStyle(
+                          color: Colors.grey.shade500,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 12,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         ),
+
+        //Botão de add carrinho
         Positioned(
           top: 4,
           right: 4,
